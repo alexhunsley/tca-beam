f9a7003545863174a677604bcc77b7cd112075ca
Alex Hunsley <alex.hunsley@gmail.com>
2023-06-04 23:45:25 +0100

    Write to files instead of stdout, add force-overwrite flag, and two-files (unimplemented)

--- Python/tca_scaffold/tca_scaffold.py
+++ Python/tca_scaffold/tca_scaffold.py
@@ -1,7 +1,9 @@
 import os
 from jinja2 import Environment, FileSystemLoader
+import click
+import sys
 
-def process_template(script_dir):
+def process_template(script_dir, two_files, force_overwrite, feature_name):
 
     os.chdir(script_dir)
 
@@ -11,9 +13,7 @@ def process_template(script_dir):
     # Create the environment
     env = Environment(loader=file_loader)
 
-
     # Prepare the substitutions
-    feature_name = "Thingo"
     substitutions = {
         'viewName': f"{feature_name}View",
         'featureName': f"{feature_name}ViewFeature"
@@ -34,18 +34,55 @@ def process_template(script_dir):
     }
 
     # Render the template with the substitutions
-    output = one_file_template.render(main_file_substitutions)
+    stub_contents = one_file_template.render(main_file_substitutions)
 
     # When saving to disk, make the single file one end with View, not ViewFeature!
-    print(output)
+    # print(output)
+
+    # Open the file for writing
+    filename = f"{feature_name}View.swift"
+    print(f"Creating stub for {feature_name}")
+
+    if os.path.isdir(filename):
+        print(f'Output destination "{filename}" already exists and is a directory, refusing to overwrite.')
+        print()
+        sys.exit()
+
+    if not force_overwrite and os.path.isfile(filename):
+        print(f'Output destination "{filename}" already exists, refusing to overwrite. Use --force-overwrite to ignore existing files.')
+        print()
+        sys.exit()
+
+    with open(filename, 'w') as f:
+        f.write(stub_contents)
 
     # output = view_feature_template.render(substitutions)
     # print(output)
 
+@click.command(no_args_is_help=True)
+@click.option('--two-files', is_flag=True, help='Put view and reducer into separate files')
+@click.option('--force-overwrite', is_flag=True, help='Force overwriting any existing files')
+@click.argument('feature_names', nargs=-1)
+def start(two_files, force_overwrite, feature_names):
+    # print(f"Args: {feature_names}")
 
-def start():
+    if len(feature_names) < 1:
+        print()
+        print("This command must be given one or more feature name arguments (after any option flags).")
+        print()
+
+        echo = click.echo
+        echo(start.get_help(click.get_current_context()))
+        sys.exit(1)
+
     script_dir = os.path.abspath(os.path.dirname(__file__))
-    process_template(script_dir)
+
+    for feature_name in feature_names:
+        process_template(script_dir, two_files, force_overwrite, feature_name)
+
+    print()
+    print("Done")
+    print()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
