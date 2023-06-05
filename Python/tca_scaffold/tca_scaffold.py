5a103692a802d124d6b8beb387f98b0f0790ae31
Alex Hunsley <alex.hunsley@gmail.com>
2023-06-05 14:07:08 +0100

    Tidy

--- Python/tca_scaffold/tca_scaffold.py
+++ Python/tca_scaffold/tca_scaffold.py
@@ -1,18 +1,19 @@
 import os
+import sys
+from pathlib import Path
 from collections import namedtuple
 from jinja2 import Environment, FileSystemLoader
 import click
-import sys
-from pathlib import Path
 
 # TODO:
-# -[ ] impl two files
-# -[ ] flags for 'features in subdirs'
-# -[ ] bump, upload to real pyPi
-# -[ ] make bbtest for it
+# -[ ] upload to main pyPi
+# -[ ] make bbtests
+# -[x] option for preview-all
+# -[x] impl two files
+# -[x] flags for 'features in sub_dirs'
 # -[x] make script use abs path to the templates, no chdir!
 # -[x] --dry-run
-# -[ ] optional allPreviews view
+
 TemplateRender = namedtuple('TemplateRender', ['render_file', 'template', 'target_dir'],
                             defaults=['', '', '.'])
 
@@ -22,7 +23,7 @@ def dbg(string):
     # print(string)
 
 
-def p(string):
+def p(string = ""):
     print(string)
 
 
@@ -35,14 +36,14 @@ def make_abs_path(rel_path):
     return os.path.normpath(os.path.join(script_dir, rel_path))
 
 
-def render_templates(templateRenders, substitutions, feature_name, two_files, dry_run, force_overwrite):
+def render_templates(templateRenders, substitutions, step_name, feature_name, two_files, dry_run, force_overwrite):
 
     console_prefix = "- (DRY RUN:) " if dry_run else "- "
 
     dbg(f"-------- render_templates: templateRenders = {templateRenders}")
     dbg(f"-------- render_templates: substitutions = {substitutions}")
 
-    p(f"{console_prefix}Feature '{feature_name}':")
+    p(f"{console_prefix}{step_name}")
 
     for templateRender in templateRenders:
 
@@ -98,18 +99,17 @@ def process_template(env, two_files, sub_dirs, force_overwrite, dry_run, feature
 
     dbg(f"just made target_dir: {target_dir}, and sub_dirs == {sub_dirs}")
     if two_files:
-        # f"{feature_name}View.swift"
-
         template_renders.append(TemplateRender(f"{feature_name}ViewFeature.swift", env.get_template('TwoFile_ReducerPart.swift'), target_dir))
         template_renders.append(TemplateRender(f"{feature_name}View.swift", env.get_template('TwoFile_ViewPart.swift'), target_dir))
     else:
         # for reducer + view in one file, we append just 'View' to feature name
         template_renders.append(TemplateRender(f"{feature_name}View.swift", env.get_template('OneFile.swift'), target_dir))
 
-    render_templates(template_renders, substitutions, feature_name, two_files, dry_run, force_overwrite)
+    step_name = f"Feature '{feature_name}':"
+    render_templates(template_renders, substitutions, step_name, feature_name, two_files, dry_run, force_overwrite)
 
 
-def generate_all_previews(env, feature_names, script_dir, two_files, sub_dirs, preview_all, force_overwrite, dry_run, feature_name):
+def generate_all_preview(env, feature_names, script_dir, two_files, sub_dirs, preview_all, force_overwrite, dry_run, feature_name):
 
     # a single View that has a preview for all the Views
     all_previews_substitutions = []
@@ -123,10 +123,8 @@ def generate_all_previews(env, feature_names, script_dir, two_files, sub_dirs, p
     substitutions_all_previews = { 'allFeatures': all_previews_substitutions}
 
     template_render = TemplateRender(f"AllPreviews.swift", env.get_template('AllPreviews.swift'))
-        # ['render_file', 'template', 'substitutions', 'target_dir'],
 
-    # template_renders.append(TemplateRender(f"AllPreviewsView.swift", env.get_template('AllPreviews.swift'), all_previews_substitutions))
-    render_templates([template_render], substitutions_all_previews, feature_name, two_files, dry_run, force_overwrite)
+    render_templates([template_render], substitutions_all_previews, 'Preview for all features:', feature_name, two_files, dry_run, force_overwrite)
 
 
 def run(env, script_dir, two_files, sub_dirs, preview_all, force_overwrite, dry_run, feature_names):
@@ -134,11 +132,11 @@ def run(env, script_dir, two_files, sub_dirs, preview_all, force_overwrite, dry_
         process_template(env, two_files, sub_dirs, force_overwrite, dry_run, feature_name)
 
     if preview_all:
-        generate_all_previews(env, feature_names, script_dir, two_files, sub_dirs, preview_all, force_overwrite, dry_run, feature_name)
+        generate_all_preview(env, feature_names, script_dir, two_files, sub_dirs, preview_all, force_overwrite, dry_run, feature_name)
 
-    print()
-    print("Done")
-    print()
+    p()
+    p("Done")
+    p()
 
 
 @click.command(no_args_is_help=True)
@@ -151,13 +149,17 @@ def run(env, script_dir, two_files, sub_dirs, preview_all, force_overwrite, dry_
 def start(two_files, sub_dirs, preview_all, force_overwrite, dry_run, feature_names):
 
     if len(feature_names) < 1:
-        print()
-        print("This command must be given one or more feature name arguments (after any option flags).")
-        print()
+        p()
+        p("Please give one or more feature name arguments (after any option flags).")
+        p()
 
         echo = click.echo
         echo(start.get_help(click.get_current_context()))
         sys.exit(1)
+
+    p("")
+    p("tca-beam is preparing two-by-fours...")
+    p("")
 
     script_dir = os.path.abspath(os.path.dirname(__file__))
 
@@ -168,8 +170,5 @@ def start(two_files, sub_dirs, preview_all, force_overwrite, dry_run, feature_na
     run(env, script_dir, two_files, sub_dirs, preview_all, force_overwrite, dry_run, feature_names)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     start()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
