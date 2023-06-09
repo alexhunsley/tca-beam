cdde877e7d0596b4424fb598aba50163b945390a
Alex Hunsley <alex.hunsley@gmail.com>
2023-06-09 10:49:35 +0100

    Make allPreviews do the right init for the HOR view (subReducerFeatures data not provided from the code yet though). This will probably be removed!

--- Python/tca_beam/templates/AllPreviews.swift
+++ Python/tca_beam/templates/AllPreviews.swift
@@ -8,17 +8,25 @@ public struct AllPreviews_Preview: PreviewProvider {
     public static var previews: some View {
         VStack {
             Spacer()
-{% for feature in allFeatures %}
+{%- for feature in allFeatures %}
             {{ feature.viewName }}(store: StoreOf<{{ feature.featureName }}>(
-                initialState: .init()) {
-                    {{ feature.featureName }}()
-                }
+{%- if subReducerFeatures|length > 0 -%}
+        initialState: .init(
+ {%- for subFeatureKey, subFeatureValue in subReducerFeatures.items() %}
+            {{ subFeatureValue.varName }}: {{ subFeatureValue.featureName }}.State(){{ ", " if not loop.last else "" }}
+ {%- endfor %}
+        )) {
+{%- else -%}
+            initialState: .init()) {
+{%- endif %}
+                {{ feature.featureName }}()
+            }
 //                 withDependencies: {
 //                     $0.someDependency = something
 //                 }
             )
             Spacer()
-{% endfor %}
+{%- endfor %}
        }
     }
 }
