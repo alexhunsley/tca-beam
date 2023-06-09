a72d44efa885bc00db8a2737d95b1c8ca193a585
Alex Hunsley <alex.hunsley@gmail.com>
2023-06-09 11:00:52 +0100

    Revert "Make all previews do..." (cdde87): we don't show preview for first feature in HOR mode anyway

--- Python/tca_beam/templates/AllPreviews.swift
+++ Python/tca_beam/templates/AllPreviews.swift
@@ -10,17 +10,9 @@ public struct AllPreviews_Preview: PreviewProvider {
             Spacer()
 {%- for feature in allFeatures %}
             {{ feature.viewName }}(store: StoreOf<{{ feature.featureName }}>(
-{%- if subReducerFeatures|length > 0 -%}
-        initialState: .init(
- {%- for subFeatureKey, subFeatureValue in subReducerFeatures.items() %}
-            {{ subFeatureValue.varName }}: {{ subFeatureValue.featureName }}.State(){{ ", " if not loop.last else "" }}
- {%- endfor %}
-        )) {
-{%- else -%}
-            initialState: .init()) {
-{%- endif %}
-                {{ feature.featureName }}()
-            }
+                initialState: .init()) {
+                    {{ feature.featureName }}()
+                }
 //                 withDependencies: {
 //                     $0.someDependency = something
 //                 }
